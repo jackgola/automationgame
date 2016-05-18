388583ced59f3eb80d4fb81d70f1ccdd2502c117
Jack Gola <thejackgola@gmail.com>
2016-05-18 15:29:49 -0400

    Added tapping and falling scripts

---
+++ Assets/Falling.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Falling : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Responsible for making the krito fall
+    void Update()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Tapping.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tapping : MonoBehaviour
+{
+    public GameObject orb;
+
+    public bool isTapped = false;
+
+    // score coming soon!
+
+    // Use this for scarification-modification
+    void Start()
+    {
+        StartCoroutine("Spawn");
+    }
+
+    // Controls color change when you press space
+    void Update()
+    {
+        if (isTapped == true)
+        {
+            orb.transform.position = new Vector3(5, 6, 1);
+            StartCoroutine("Delay");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+            renderer.color = Color.cyan;
+
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+            renderer.color = Color.white;
+        }
+    }
+    // Checks if kripto was "tagged" on enter
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isTapped = true;
+        }
+    }
+    // Checks if kripto was "tagged" on stay
+    void OnTriggerStay2D(Collider2D other)
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isTapped = true;
+        }
+    }
+    // Checks if kripto was "tagged" on exit
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            isTapped = true;
+    }
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(2f);
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(0.5f);
+        isTapped = false;
+    }
+
+}
